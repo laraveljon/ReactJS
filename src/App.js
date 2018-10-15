8dc4c45d48ea15041f2f6d00bb91fd5f9d843d4c
Jonathan <jonathan_alanis@outlook.com>
2018-10-14 21:53:49 -0500

    Primer summit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+
+        
+         <h1>Body</h1>
+         
+        );
+    }
+}
+
+export default App;
