fe428bab7147e76451da8b06e47efbc238583e0c
Jonathan <jonathan_alanis@outlook.com>
2018-10-14 23:22:45 -0500

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function  Hello(props){
+    return <h1>Componente</h1>
+}
+
+
+function  Hello2(props){
+    return <h2>{props.title}</h2>
+}
+
 class App extends Component {
     render() {
         return (
-
-        
-         <h1>Body</h1>
+            <div className="App">
+            
+              <img src={logo} className="App-logo" alt="logo" />
+                <Hello/>
+                <Hello2 title='Hello from props' />
+                Learn React
+          
+           
+          </div>
+  
          
         );
     }
